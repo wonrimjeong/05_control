8af9b096ed44b3d807a83336d2466699f303123a
wonrimjeong <wonrim0802@gmail.com>
2018-10-11 16:38:30 +0900

    05project5

--- main.c
+++ main.c
@@ -5,22 +5,32 @@
 
 int main(int argc, char *argv[]) {
 	
-	int num=0;
-	char c;
-	char str[100];
+	int answer = 59;
+	int i;
+	int num=1;
 	
-	//string
-	printf("string :");
-	//scanf("%s");
-	
-	while ((c=getchar())  != '\n')
-	{
-	    if ('0' <= c && '9'>= c)
+	do {
+		printf("input a number : ");
+		scanf("%d", &i);
+		
+		if(i < answer)
 		{
+			printf("low!\n");
 			num++;
-		}	
-	}
-	printf("The number of digits are %d", num);
+		}
+		else if (i > answer)
+		{
+			printf("high!\n");
+			num++;
+		}
+		else
+		{
+			printf("Á¤´ä!\n");
+		}
+		
+	} while (i !=answer);
+	
+	printf("trial : %i\n", num);
 	
 	return 0;
 }
